3db9cb18294eac8cf1cb673b827f5cb7d4bb1451
opentlc-mgr <lilim@redhat.com>
2022-02-25 05:08:03 +0000

    first commit

---
+++ random-torch.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------------------
+#  Copyright (c) Red Hat, Inc. All rights reserved.
+#  Licensed under the MIT License. See LICENSE in the project root for license information.
+#--------------------------------------------------------------------------------------------
+
+# This program prints Hello, world!
+
+import torch
+x = torch.rand(5, 3)
+print(x)
